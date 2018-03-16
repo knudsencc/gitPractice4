cfb3f4637a87700bbcf97621b2d9856f9bc62b0e
Cameron KNUDSEN <tmphotock@gmail.com>
2018-03-16 11:30:26 -0600

    Cameron Made Changes

--- code_time.py
+++ code_time.py
@@ -1,3 +1,9 @@
 print("hewow world')
       
 print("here's another print statement")
+
+def my changes():
+    print("I think this is right???")
+
+def my changes_2():
+    print("I hope this is right???")
